60f1b82d61615b2ab94b95d343bdb5998c21942e
VitaliaClinic <brendacumpri@gmail.com>
2025-06-15 19:41:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1 style={{ color: '#0E78BF' }}>Vitalia Clinic</h1>
+      <p>Saúde com ciência, empatia e movimento.</p>
+      <button style={{ backgroundColor: '#0E78BF', color: '#fff', padding: '0.5rem 1rem', border: 'none', borderRadius: '5px' }}>
+        Agende sua consulta
+      </button>
+    </div>
+  );
+}
